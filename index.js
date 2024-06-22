80ec976d58a5b2efaeba09453b8e112aea07260a
Mateus Bittencourt <106128644+mateus-bittencourt-cc@users.noreply.github.com>
2024-06-22 14:39:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+</head>
+<body>
+    <h2>Bem-vindo à Página Inicial</h2>
+    <button onclick="location.href='/add-product.html'">Adicionar Produto</button>
+    <button onclick="location.href='/dashboard.html'">Dashboard</button>
+</body>
+</html>
